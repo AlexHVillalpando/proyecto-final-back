84c7e2f687d27d9d7dd09f4e47f23b80ed27524d
Oscar Alejandro Huerta Villalpando <42473462+AlexHVillalpando@users.noreply.github.com>
2025-02-20 18:31:12 -0600

    Added-app.ts

---
+++ src/app.ts
@@ -0,0 +1,28 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import { envs } from "./config/env";
+import { PostgresDataBase } from "./data";
+import { AppRoutes } from "./presentation/routes";
+import { Server } from "./presentation/server";
+
+async function main() {
+
+  const postgres = new PostgresDataBase({
+    username: envs.DB_USERNAME,
+    password: envs.DB_PASSWORD,
+    host: envs.DB_HOST,
+    database: envs.DB_DATABASE,
+    port: envs.DB_PORT
+})
+await postgres.connect()
+
+  const server = new Server({
+    port: envs.PORT,
+    routes: AppRoutes.routes
+  });
+
+  await server.start();
+}
+
+main();
